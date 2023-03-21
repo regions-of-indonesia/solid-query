17b55fffa2ded4abe5aa76d51dff0307bbfd8f05
flamrdevs <flamrdevs@gmail.com>
2023-03-22 06:08:09 +0700

    Add name options

--- test/Component.tsx
+++ test/Component.tsx
@@ -2,18 +2,22 @@ import { For } from "solid-js";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 
-import { createProvinces } from "./regions-of-indonesia";
+import { createProvinces, createDistricts, createSubdistricts, createVillages } from "./regions-of-indonesia";
 
 const AsyncComponent = () => {
-  const query = createProvinces();
+  const provinces = createProvinces();
+  const districts = createDistricts(() => "11");
+  const subdistricts = createSubdistricts(() => "11.01");
+  const villages = createVillages(() => "11.01.01");
 
   return (
     <div>
-      <For each={query.data || []}>
-        {(item) => {
-          return <div data-testid={`code-${item.code}`}>{item.name}</div>;
-        }}
+      <For each={provinces.data || []}>{(province) => <div data-testid={`province-${province.code}`}>{province.name}</div>}</For>
+      <For each={districts.data || []}>{(district) => <div data-testid={`district-${district.code}`}>{district.name}</div>}</For>
+      <For each={subdistricts.data || []}>
+        {(subdistrict) => <div data-testid={`subdistrict-${subdistrict.code}`}>{subdistrict.name}</div>}
       </For>
+      <For each={villages.data || []}>{(village) => <div data-testid={`village-${village.code}`}>{village.name}</div>}</For>
     </div>
   );
 };
--- test/regions-of-indonesia.ts
+++ test/regions-of-indonesia.ts
@@ -2,6 +2,8 @@ import { RegionsOfIndonesiaClient } from "@regions-of-indonesia/client";
 
 import { createSolidQuery } from "../src";
 
-const { createProvinces } = createSolidQuery(new RegionsOfIndonesiaClient({ baseURL: { dynamic: "http://127.1.0.0:8000" } }));
+const { createProvinces, createDistricts, createSubdistricts, createVillages } = createSolidQuery(
+  new RegionsOfIndonesiaClient({ baseURL: { dynamic: "http://127.1.0.0:8000" } })
+);
 
-export { createProvinces };
+export { createProvinces, createDistricts, createSubdistricts, createVillages };
